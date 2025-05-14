d60aad15fda2740a0748b3568dd4d449ad896824
Joao Felipe Galvao <joaofelipe.galvao021@gmail.com>
2025-05-14 11:14:08 -0300

    feat: implementar agenda de contatos com operações básicas
    - Criar classe AgendaContatos utilizando Map
    - Implementar métodos para:
      - Adicionar contato (com sobrescrita)
      - Remover contato
      - Exibir todos os contatos
      - Pesquisar número por nome
    - Adicionar exemplos de uso no método main

---
+++ programacao-orientada-a-objetos/collections-java-api/src/map/OperacoesBasicas/AgendaContatos.java
@@ -0,0 +1,52 @@
+package map.OperacoesBasicas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+   private Map<String, Integer> agendaContatoMap;
+
+   public AgendaContatos() {
+      this.agendaContatoMap = new HashMap<>();
+   }
+
+   public void adicionarContato(String nome, Integer telefone) {
+      agendaContatoMap.put(nome, telefone);
+   }
+
+   public void removerContato(String nome) {
+      if (!agendaContatoMap.isEmpty()) {
+         agendaContatoMap.remove(nome);
+      }
+   }
+
+   public void exibirContatos() {
+      System.out.println(agendaContatoMap);
+   }
+
+   public Integer pesquisarPorNome(String nome) {
+      Integer numeroPorNome = null;
+      if (!agendaContatoMap.isEmpty()) {
+         numeroPorNome = agendaContatoMap.get(nome);
+      }
+      return numeroPorNome;
+   }
+
+   public static void main(String[] args) {
+      AgendaContatos agendaContatos = new AgendaContatos();
+
+      agendaContatos.adicionarContato("João", 123456);
+      agendaContatos.adicionarContato("João", 5665);
+      agendaContatos.adicionarContato("João Felipe", 1111111);
+      agendaContatos.adicionarContato("João DIO", 654987);
+      agendaContatos.adicionarContato("Maria Silva", 1111111);
+      agendaContatos.adicionarContato("João", 44444);
+
+      agendaContatos.exibirContatos();
+
+      agendaContatos.removerContato("Maria Silva");
+      agendaContatos.exibirContatos();
+
+      System.out.println("O número é: " + agendaContatos.pesquisarPorNome("João DIO"));
+   }
+}
